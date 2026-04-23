f2318f1fc0de835b649748941c4854089b41e726
waises <44254125+waises@users.noreply.github.com>
2026-04-23 13:38:06 +0700

    Add 'README.md' to ignorePatterns in config

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "waises.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "README.md"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
