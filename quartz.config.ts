7f5491b6cde3b1a5ba1ab5f7371f3e083ebd40e3
waises <kkpavelp@gmail.com>
2026-04-24 21:46:45 +0300

    changed SPA

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Pavel's pages",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
